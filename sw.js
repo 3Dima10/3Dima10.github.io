8d8c7536bba9ab8e275db96035f8e264f3f23f17
3Dima10 <112176437+3Dima10@users.noreply.github.com>
2025-02-17 01:38:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ self.addEventListener('install', event => {
                 '/index.html',
                 '/style.css',
                 '/script.js',
-                '/Designer2.png'
+                '/logo.png'
             ]);
         })
     );
